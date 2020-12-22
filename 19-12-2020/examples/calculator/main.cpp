59a53d147c22e6fc81353b050baf051aabded5a9
Mahendra Garodi <garodimb@gmail.com>
2020-12-22 09:41:33 +0530

    Added slides and examples for CMake + Conan Part 1

---
+++ 19-12-2020/examples/calculator/main.cpp
@@ -0,0 +1,8 @@
+#include "calculator.h"
+
+int main(void)
+{
+	Calculator cal;
+	std::cout << "Sum of 10 + 20: " << cal.add(10,20) << std::endl;
+	return 0;
+}
---
+++ 19-12-2020/examples/calculator/src/calculator.cpp
@@ -0,0 +1,18 @@
+#include "calculator.h"
+
+int Calculator::add(int num1, int num2) 
+{
+	return num1 + num2;
+}
+int Calculator::subtract(int num1, int num2)
+{
+	return num1 - num2;
+}
+int Calculator::multiply(int num1, int num2)
+{
+	return num1 * num2;
+}
+int Calculator::divide(int num1, int num2)
+{
+	return num1 / num2;
+}
